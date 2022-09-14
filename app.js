caedea5d0b3d2608d1d04726be17216e787c3f69
namiqmamedov <namigmm@code.edu.az>
2022-09-14 12:11:17 +0400

    JS-5

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var btn_add = document.getElementById('btn_add');
 btn_add.onclick = function(){
     var value = document.getElementById('input').value;
     var h3 = document.createElement('h3');
+    var icon = document.createElement('i');
+    var div = document.createElement('div');
+    div.setAttribute('class', 'box');
+    icon.setAttribute('class','fa-solid fa-trash')
     h3.innerHTML = value
-    h3.setAttribute('class','p228')
+    // h3.setAttribute('class','p228')
     var todo = document.getElementById('todos');
-    todo.appendChild(h3)
+    div.appendChild(h3)
+    div.appendChild(icon)
+    todo.appendChild(div)
 }
